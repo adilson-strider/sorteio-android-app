a440603d7fbd80ece873df76dcaba8c7a1e5a884
adilson-strider <adilsonjunior.dev01@gmail.com>
2024-08-22 01:06:48 -0300

    Initial commit

---
+++ app/src/main/java/com/strider/sorteio/MainActivity.java
@@ -0,0 +1,31 @@
+package com.strider.sorteio;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void drawNumber(View view){
+
+        TextView text = findViewById(R.id.textResult);
+
+        Random rand = new Random();
+
+        text.setText(String.valueOf(rand.nextInt(11)));
+    }
+}
